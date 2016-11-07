b44be9d2302c0cf243ede74a5fb7fcd347a4b6a8
maxxporoshin <maxxporoshin@gmail.com>
2016-11-07 20:50:22 +0300

    code simplified

--- __main__.py
+++ __main__.py
@@ -2,21 +2,8 @@ import random
 import itertools
 
 
-class Station:
-    def __init__(self, index, trains=[]):
-        self.index = index
-        self.trains = trains
-
-
-def print_stations(S):
-    for station in S:
-        print(station.index, station.trains)
-
-
 def generate_graph(n, m, p):
-    G = [Station(i) for i in range(n)]
-    for station in G:
-        station.trains = [1 if random.random() < p else 0 for j in range(m)]
+    G = [[1 if random.random() < p else 0 for j in range(m)] for i in range(n)]
     return G
 
 
@@ -26,25 +13,23 @@ def generate_pairs(amount, n):
 
 
 def check_pairs(L, S):
-    indeces = []
-    for station in S:
-        indeces.append(station.index)
     for pair in L:
-        if pair[0] in indeces and pair[1] in indeces:
+        if pair[0] in S and pair[1] in S:
             return False
     return True
 
 
 def bruteforce(G, L):
-    m = len(G[0].trains)
-    for k in range(2, len(G) + 1):
-        subsets = itertools.combinations(G, k)
+    n = len(G)
+    m = len(G[0])
+    for k in range(2, n + 1):
+        subsets = itertools.combinations(range(n), k)
         for S in subsets:
             all_connected = True
             for j in range(m):
                 connected = False
-                for station in S:
-                    if station.trains[j] == 1:
+                for i in range(n):
+                    if G[i][j] == 1:
                         connected = True
                 if not connected:
                     all_connected = False
@@ -58,11 +43,11 @@ def generate_and_solve_problem(n, m, p, l_amount):
     G = generate_graph(n, m, p)
     L = generate_pairs(1, n)
     print('G:')
-    print_stations(G)
+    print(G)
     print('L:')
     print(L)
     print('Bruteforce:')
-    print_stations(bruteforce(G, L))
+    print(bruteforce(G, L))
 
 
 generate_and_solve_problem(3, 5, 0.5, 1)
