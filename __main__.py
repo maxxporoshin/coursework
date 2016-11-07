0f7270ada742e9077eb988e47273142c640502c8
maxxporoshin <maxxporoshin@gmail.com>
2016-11-07 23:29:04 +0300

    improved bruteforce

--- __main__.py
+++ __main__.py
@@ -19,35 +19,91 @@ def check_pairs(L, S):
     return True
 
 
-def bruteforce(G, L):
+def check_solvability_conditions(G, L):
     n = len(G)
     m = len(G[0])
-    for k in range(2, n + 1):
+    for j in range(m):
+        stations = []
+        for i in range(n):
+            if G[i][j] == 1:
+                stations.append(i)
+        if len(stations) == 0:
+            return False
+    return True
+
+
+def find_essential_subset(G):
+    n = len(G)
+    m = len(G[0])
+    essentials = []
+    for j in range(m):
+        stations = []
+        for i in range(n):
+            if G[i][j] == 1:
+                stations.append(i)
+        if len(stations) == 1:
+            if stations[0] not in essentials:
+                essentials.append(stations[0])
+    return essentials
+
+
+def check_subset(S, G, L):
+    m = len(G[0])
+    all_connected = True
+    for j in range(m):
+        connected = False
+        for i in S:
+            if G[i][j] == 1:
+                connected = True
+        if not connected:
+            all_connected = False
+    if all_connected:
+        if check_pairs(L, S):
+            return True
+    return False
+
+
+def bruteforce(G, L):
+    n = len(G)
+    for k in range(1, n + 1):
         subsets = itertools.combinations(range(n), k)
         for S in subsets:
-            all_connected = True
-            for j in range(m):
-                connected = False
-                for i in range(n):
-                    if G[i][j] == 1:
-                        connected = True
-                if not connected:
-                    all_connected = False
-            if all_connected:
-                if check_pairs(L, S):
-                    return S
+            if check_subset(S, G, L):
+                return list(S)
+    return []
+
+
+def improved_bruteforce(G, L):
+    n = len(G)
+    if not check_solvability_conditions(G, L):
+        return []
+    essentials = find_essential_subset(G)
+    if not check_pairs(L, essentials):
+        return []
+    if check_subset(essentials, G, L):
+        return essentials
+    remaining = [i for i in range(n) if i not in essentials]
+    for k in range(1, len(remaining) + 1):
+        subsets = itertools.combinations(remaining, k)
+        for subset in subsets:
+            S = list(subset)
+            S.extend(essentials)
+            if check_subset(S, G, L):
+                return S
     return []
 
 
 def generate_and_solve_problem(n, m, p, l_amount):
     G = generate_graph(n, m, p)
-    L = generate_pairs(1, n)
+    L = generate_pairs(l_amount, n)
     print('G:')
     print(G)
     print('L:')
     print(L)
     print('Bruteforce:')
     print(bruteforce(G, L))
+    print('Improved bruteforce:')
+    print(improved_bruteforce(G, L))
 
 
 generate_and_solve_problem(3, 5, 0.5, 1)
